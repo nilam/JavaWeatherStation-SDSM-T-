bb8977df9bb74246cb990bc1daf422097164f71a
Joshua Kinkade <joshua.kinkade@mines.sdsmt.edu>
2011-11-09 12:33:02 -0700

    Added DataCalculator to find averages and minimums and maximums

---
+++ gui/pa2/DataCalculator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui.pa2;
+
+/**
+ *
+ * @author jmk
+ */
+public class DataCalculator {
+    
+    
+}
